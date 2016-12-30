8917c2eeaf1b161fd951c79fd3c09764b5656451
Tobias Hvass Mølbak <tobiashmhtx@gmail.com>
2016-12-30 15:13:38 +0100

    Finished CSS - built background - working on upper include

--- historie.php
+++ historie.php
@@ -1,8 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>NLW</title>
+  
+  <link rel="shortcut icon" href="/images/favicon.ico"/>
+  <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
+  <!-- Optional Bootstrap theme -->
+  <link rel="stylesheet" href="dist/css/custom-bootstrap.css">
+</head>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<body>
+  <?php
+  include 'init.php';
+  include ROOT_DIR . '/includes/upper-body.php';?>
 
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        <h2 class="panel-title">Historien om NLW</h2>
+      </div>
+        <div class="panel-body">
+          <p>Nøvling Lan Week er oprindeligt et arrangement, som foreningen Nøvling Ungdoms Forening, dannet sommeren 2007, stod for. De 5 oprindelige grundlæggere; Mathias Huse Jensen, Tobias Hvass Mølbak, Kristoffer Nygaard Jensen, Rasmus Vollstedt og Jacob Andersen, påbegyndte pojektet i Nøvling Festuge (uge 36) '07, som dengang manglede et arrangement for de unge i byen.</p>
+          <p>Til trods for de enkle rammer (Nøvling Forsamlingshus) uden internet og udstyr, afholdt den lille forening det første LAN, som blev en stor success. Nøvling LAN Week fortsatte som selvstændigt arrangement de følgende år, hvor der blev opgraderet på udstyret og justeret på nettet.</p>
+          <p>I 2013 kom der en ny generation til og med 2 af grundlæggerne i ryggen, står de nu for de årlige arrangementer.</p>
+        </div>
+    </div>
+  
+
+  <?php include ROOT_DIR . '/includes/lower-body.php';?>
+</body>
+</html>
--- kontakt.php
+++ kontakt.php
@@ -1,8 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>NLW</title>
+  
+  <link rel="shortcut icon" href="/images/favicon.ico"/>
+  <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
+  <!-- Optional Bootstrap theme -->
+  <link rel="stylesheet" href="dist/css/custom-bootstrap.css">
+</head>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<body>
+  <?php
+  include 'init.php';
+  include ROOT_DIR . '/includes/upper-body.php';?>
 
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        <h3 class="panel-title">Kontakt Nøvling Lan Week</h3>
+      </div>
+      <div class="panel-body">
+        <p><b>Adresse:</b> Opalvej 4, 9260 Gistrup</p>
+        <p><b>Telefon:</b> 60 45 30 35</p>
+        <p><b>Mail:</b> NovlingLanWeek@gmail.com</p>
+        <p><b>Facebook:</b> <a href="https://www.facebook.com/NovlingLANWeek">Nøvling Lan Week - NLW</a></p>
+      </div>
+    </div>
+
+  <?php include ROOT_DIR . '/includes/lower-body.php';?>
+</body>
+</html>
