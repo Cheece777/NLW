7107d9f95f08b7330bfd6cee5df39dc02f86c5d7
Tobias Mølbak <tobias@molbak.org>
2022-11-10 21:03:40 +0100

    Updated Minecraft and TrackMania

--- nlw.dk/konkurrence/minecraft.php
+++ nlw.dk/konkurrence/minecraft.php
@@ -16,29 +16,26 @@
   <div class="panel panel-primary">
     <div class="panel-heading banner-minecraft"></div>
     <div class="panel-body">
-      <p>Få chance for at bevise at du kan bygge den mest fantastiske skulptur, personlighed eller noget helt tredje ved NLW's 24 timers Minecraft turnering!</p>
+      <p>Vis dine evner som den bedste Minecraft kæmper på tværs af en række forskellige udfordringer og konkurrencer!</p>
     </div>
-    <div class='panel-heading'>§1 Format</div>
+    <div class='panel-heading'>§1 Regler</div>
     <ul class="list-group">
       <li class="list-group-item">§1.1 Alle mod alle.</li>
-      <li class="list-group-item">§1.2 Der skal bygges en figur eller andet inden for de afmærkede områder, som vises frem når turneringen slutter.</li>
-      <li class="list-group-item">§1.3 Vinderen er den deltager, hvis figur får flest stemmer.</li>
+      <li class="list-group-item">§1.2 Når banen går igang spreder alle deltagerne sig ud på banen, og venter med at angribe hinanden til der står "GO" på skræmen. Det vil være en tydlig timer midt på skræmen hvor den tæller ned fra 3-2-1, og så er det GO.</li>
+      <li class="list-group-item">§1.3 Der vil blive kørt test på dette, at alle deltager kan se "3-2-1 Go". Hvis det er man ikke kan se det, skal man hurtigt henvende sig til Crew.</li>
+      <li class="list-group-item">§1.4 Efter 3 minutter midt i kampen vil alle blive highlighted, med Glowing, så man kan se hinanden, og det vil så være kamp til døden.</li>
+      <li class="list-group-item">§1.5 Alle banerne er kørt 1 gang, vil der blive highlighted på sort skærmen hvem der fører første runde.</li>
+      <li class="list-group-item">§1.6 Alle baner bliver spillet 2 gange, og tager som minimum 1 til 2 minutter.</li>
     </ul>
-    <div class='panel-heading'>§2 Regler</div>
+    <div class='panel-heading'>§2 Point system</div>
     <ul class="list-group">
-      <li class="list-group-item">§2.1 Ingen snyd! (Jo vi kan godt se, hvis du bruger en botter).</li>
-      <li class="list-group-item">§2.2 Man må ikke ødelægge hinandens. Hvis du ødelægger eller på anden måde sabotere en anden deltagers figur bliver du diskvalificeret.</li>
-      <li class="list-group-item">§2.3 Hold dig til dig selv! (De indrettede firkanter).</li>
-      <li class="list-group-item">§2.4 Der er kun 1 firkant per deltager.</li>
+      <li class="list-group-item"><b>10</b> Points for at være den sidste i live.</li>
+      <li class="list-group-item"><b>3</b> Points for kills.</li>
+      <li class="list-group-item">Der vil være et scoreboard til højre på skræmen, hvor folk kan følge med i deres kills.</li>
     </ul>
-    <div class='panel-heading'>§3 Tilmelding</div>
+    <div class='panel-heading'>§3 Præmie</div>
     <ul class="list-group">
-      <li class="list-group-item">§3.1 Minecraft serveren åbner kort efter NLW's start, og kræver ingen tilmelding.</li>
-      <li class="list-group-item">§3.2 Turneringen afslutter 24 timer senere, i takt med aftensmaden bliver severet om lørdagen.</li>
-    </ul>
-    <div class='panel-heading'>§4 Præmie</div>
-    <ul class="list-group">
-      <li class="list-group-item">1. pladsen En NLW Natmenu (et gavekort á 5 toasts og 2 sodavand)</li>
+      <li class="list-group-item">Første pladsen på point tavlen vinder 250 DKK.</li>
     </ul>
   </div>
   
