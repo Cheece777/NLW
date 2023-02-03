c6b6a9e0fa6e3ea21b0217c605764d507a501010
Cheece777 <cheece_mathias@hotmail.com>
2023-02-03 20:45:55 +0100

    Opdateret title ved program til nlw32

--- nlw.dk/program.php
+++ nlw.dk/program.php
@@ -15,7 +15,7 @@
 
     <div class="panel panel-primary">
       <!-- Default panel contents -->
-        <div class="panel-heading">Program oversigt for NLW #31</div>
+        <div class="panel-heading">Program oversigt for NLW #32</div>
       <div class="panel-body">
         <ul class="nav nav-tabs">
           <li class="active"><a data-toggle="tab" href="#fredag">Fredag</a></li>
